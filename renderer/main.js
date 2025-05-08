15f50a975a2b69264fecedba8ac1a57e361ed768
imlauvi <levi.zhang3@education.nsw.gov.au>
2025-05-08 22:27:36 +1000

    added terminal resize

--- renderer/main.js
+++ renderer/main.js
@@ -43,7 +43,49 @@ function sideTabResize(){ //maybe add mobile support
     }
 }
 
+function terminalResize(){ //maybe add mobile support
+    let terminalAdjusting = false;
+
+    const terminalResize = getElement("terminal-resize");
+
+    terminalResize.addEventListener("mousedown", startResize);
+
+    function startResize(){
+        sideTabAdjusting = true;
+        terminalResize.setAttribute("resizing", null);
+
+        document.addEventListener("mousemove", resize);
+        document.addEventListener("mouseup", endResize);
+    }
+
+    function resize(event){
+        if(!sideTabAdjusting){
+            return;
+        }
+        
+        document.body.style.cursor = "ns-resize";
+
+        let size = window.innerHeight - event.clientY;
+        let terminal = window.getComputedStyle(getElement("terminal"));
+        size = Math.max(parseInt(terminal.getPropertyValue("min-height")), Math.min(size, parseInt(terminal.getPropertyValue("max-height"))))
+        document.documentElement.style.setProperty("--terminal-height", `${size}px`);
+    }
+
+    function endResize(){
+        terminalAdjusting = false;
+        if(terminalResize.hasAttribute("resizing")){
+            terminalResize.removeAttribute("resizing");
+        }
+
+        document.body.style.cursor = "default";
+
+        document.removeEventListener("mousemove", resize);
+        document.removeEventListener("mouseup", endResize);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     sideTabResize();
+    terminalResize();
 })
 
